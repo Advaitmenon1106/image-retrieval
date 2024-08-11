ac6f038cd416da319ddcc6fd102f9749e5e34876
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-11 07:47:46 +0530

    Modularise the existing code, remove some redundant code from it.

--- Scrape.py
+++ Scrape.py
@@ -1,44 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-parentUrl = 'https://www.yahoo.com/'
 import grape
-from grape.general_graph import GeneralGraph
 from grape.general_graph import nx
 import pickle
 
-graph = GeneralGraph()
+parentUrl = 'https://www.yahoo.com/'
+graph = nx.Graph()
 graph.add_node(parentUrl)
 
 c = 5
 
+def find_tag(url:str, tagName:str='a'):
+    try:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        tagList = soup.findAll(tagName)
+        return tagList
+    except:return
+
+def track_and_add_attribute(srcNode:str, tagList:list, graphObj:nx.Graph, attr:str = 'href'):
+    for i in range(0, len(tagList)):
+        if tagList[i][attr] and 'http' in tagList[i][attr]:
+            graphObj.add_edge(srcNode, tagList[i][attr])
+
+def url_bfs_traversal(graphObj:nx.Graph, srcNode:str):
+    edges = nx.breadth_first_search.bfs_edges(graphObj, source=srcNode)
+    bfs_nodes = [v for u, v in edges]
+    for node in bfs_nodes:
+        tagList = find_tag(node)
+        
+        if tagList: track_and_add_attribute(srcNode=node, tagList=tagList, graphObj=graphObj)
+        pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
+        
+        url_bfs_traversal(graphObj=graph, srcNode=node)
+        pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
+
 while c>=0:
-    r = requests.get(parentUrl)
-    soup = BeautifulSoup(r.content, 'html.parser')
-
-    imgs = soup.findAll('img')
-    anchorTags = soup.findAll('a')
-
-    sources = [imgs[i]['src'] for i in range(0, len(imgs))]
-    for i in range(0, len(anchorTags)):
-        if anchorTags[i]['href'] not in set(graph.nodes):
-            graph.add_edge(parentUrl, anchorTags[i]['href'])
-    edges = list(nx.breadth_first_search.bfs_edges(graph, source=parentUrl))
-    bfsNodes = [v for u, v in edges]
-    for child in bfsNodes:
-        if 'https' in child:
-            parentUrl = child
-            r = requests.get(parentUrl)
-            soup = BeautifulSoup(r.content, 'html.parser')
-            imgs.extend(soup.findAll('img'))
-            anchorTags = soup.findAll('a')
-            
-            for i in range(0, len(anchorTags)):
-                try:
-                    if anchorTags[i]['href'] and anchorTags[i]['href'] not in set(graph.nodes):
-                        graph.add_edge(parentUrl, anchorTags[i]['href'])
-                except:
-                    pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
-                    continue
-        else:
-            continue
+    tagList = find_tag(parentUrl)
+    if tagList: track_and_add_attribute(srcNode=parentUrl, tagList=tagList, graphObj=graph)
+    url_bfs_traversal(graphObj=graph, srcNode=parentUrl)
     c-=1
+    pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
+
+pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
