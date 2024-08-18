bea4899d735f535bb91203ae101124bf27c42d35
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-18 16:26:15 +0530

    Remove the unnecessary recursive code. Take precautions to ensure the code does not create errors if the html tags do not have an href attribute.

--- Scrape.py
+++ Scrape.py
@@ -20,26 +20,27 @@ def find_tag(url:str, tagName:str='a'):
 
 def track_and_add_attribute(srcNode:str, tagList:list, graphObj:nx.Graph, attr:str = 'href'):
     for i in range(0, len(tagList)):
-        if tagList[i][attr] and 'http' in tagList[i][attr]:
-            graphObj.add_edge(srcNode, tagList[i][attr])
+        if tagList[i].has_attr('href') and 'http' in tagList[i][attr]:
+            try:
+                graphObj.add_edge(srcNode, tagList[i][attr])
+            except:
+                continue
 
 def url_bfs_traversal(graphObj:nx.Graph, srcNode:str):
-    edges = nx.breadth_first_search.bfs_edges(graphObj, source=srcNode)
+    edges = nx.bfs_edges(graphObj, source=srcNode)
     bfs_nodes = [v for u, v in edges]
     for node in bfs_nodes:
         tagList = find_tag(node)
         
         if tagList: track_and_add_attribute(srcNode=node, tagList=tagList, graphObj=graphObj)
-        pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
-        
-        url_bfs_traversal(graphObj=graph, srcNode=node)
-        pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
+        pickle.dump(graph, open('./graph_progress2.pickle', 'wb'))
 
-while c>=0:
+
+while True:
     tagList = find_tag(parentUrl)
     if tagList: track_and_add_attribute(srcNode=parentUrl, tagList=tagList, graphObj=graph)
     url_bfs_traversal(graphObj=graph, srcNode=parentUrl)
     c-=1
-    pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
+    pickle.dump(graph, open('./graph_progress2.pickle', 'wb'))
 
-pickle.dump(graph, open('./graph_progress.pickle', 'wb'))
+pickle.dump(graph, open('./graph_progress2.pickle', 'wb'))
